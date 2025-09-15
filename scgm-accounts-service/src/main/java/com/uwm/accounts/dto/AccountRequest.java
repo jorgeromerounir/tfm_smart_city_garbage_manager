278565fe52129a81b62aea8010ff98c9651a865d
jorge.romero <jorge.romero@appgate.com>
2025-09-14 19:10:53 -0500

    :sparkles: feat(SCGM): remove accounts-service

--- scgm-accounts-service/src/main/java/com/uwm/accounts/dto/AccountRequest.java
+++
@@ -1,5 +0,0 @@
-package com.uwm.accounts.dto;
-
-import com.uwm.accounts.entity.Account;
-
-public record AccountRequest(String name, String email, String password, Account.Profile profile, String country) {}
--- scgm-accounts-service/src/main/java/com/uwm/accounts/event/AccountEvent.java
+++
@@ -1,5 +0,0 @@
-package com.uwm.accounts.event;
-
-import com.uwm.accounts.entity.Account;
-
-public record AccountEvent(String eventType, Long accountId, String email, Account.Profile profile, String password) {}
